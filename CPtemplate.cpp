2c8bccbb1306696746da998f80caeec6d54dab1f
FaizanRasoolQ <80808990+FaizanRasoolQ@users.noreply.github.com>
2022-06-30 16:22:19 +0530

    Add files via upload

---
+++ CPtemplate.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define loop(i,a,b) for(int i=a;i<=b;i++)
+#define loopr(i,a,b) for(int i=a;i>=b;i--)
+#define Loop(i,a,b) for(int i=a;i<b;i++)
+#define vi vector <int> 
+#define vl vector <ll>
+#define vc vector <char>
+#define vb vector <bool>
+#define vs vector <string>
+#define vvi vector <vector<int>>
+#define pq_max  priority_queue<ll>
+#define pq_min  priority_queue<ll, vi, greater<ll> >
+#define ump unordered_map
+#define ust unordered_set
+#define mp  make_pair
+#define all(n) n.begin(),n.end()
+#define ff first
+#define ss second
+#define pi pair<int,int>
+#define pl pair<ll,ll>
+#define mid(l,r) (l+(r-l)/2)
+#define pb  push_back
+#define bitc(n)     __builtin_popcount(n)
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) cout<<(arr[z])<<endl;
+using namespace std;
+
+void solve(){
+
+}
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+   return 0;
+}
